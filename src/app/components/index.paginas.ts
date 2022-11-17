9154de0cc9ee9cb8f91e1f93eb61088c6589bf69
JulianaR9 <julianavreyes06@gmail.com>
2022-11-17 14:03:36 -0500

    Rutas

---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,9 @@
+
+
+
+export { ContentComponent} from "./content/content.component"
+export { ClasificacionComponent } from "./clasificacion/clasificacion.component"
+export { HorariosComponent } from "./horarios/horarios.component"
+export { AlarmaComponent } from "./alarma/alarma.component"
+export { PuntosComponent} from "./puntos/puntos.component"
+export { RecoleccionComponent} from "./recoleccion/recoleccion.component"
