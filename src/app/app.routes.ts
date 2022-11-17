16c783f54d3067fa84d6aa68e45696d97746f2e8
JulianaR9 <julianavreyes06@gmail.com>
2022-11-17 17:07:57 -0500

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,13 +13,13 @@ import {
 } from "./components/index.paginas";
 
 const app_routes: Routes =  [
-    { path:'', component: ContentComponent },
+    { path:'home', component: ContentComponent },
     { path:'clasificacion', component: ClasificacionComponent },
     { path:'horarios', component: HorariosComponent },
     { path:'alarma', component: AlarmaComponent },
     { path:'puntos', component: PuntosComponent },
     { path:'recoleccion', component: RecoleccionComponent },
-    { path: '**', pathMatch: 'full', redirectTo: ''}
+    { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, { useHash:true}) ;
