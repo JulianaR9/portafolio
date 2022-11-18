8adbd639da2da4d2d0a7bb6f351aec6fb2031858
JulianaR9 <julianavreyes06@gmail.com>
2022-11-18 13:59:19 -0500

    content inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ContentComponent } from './components/content/content.component';
+import { ContentComponent} from './components/content/content.component';
 import { ClasificacionComponent } from './components/clasificacion/clasificacion.component';
 import { HorariosComponent } from './components/horarios/horarios.component';
 import { AlarmaComponent } from './components/alarma/alarma.component';
